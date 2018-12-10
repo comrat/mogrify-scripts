95ca59b87dc9074284ae3db27a8dd68f9d325bcb
Alexey Pobery <poberyaa@gmail.com>
2018-12-11 02:18:54 +0300

    Remove hint file its not needed anymore

--- create_sprite.sh
+++
@@ -1,2 +0,0 @@
-comrat@comrat-pc:~/Downloads/sprites/effects/extra_move$ montage *.png -tile 20x1 -geometry 94x127+0+0 out.png
-comrat@comrat-pc:~/Downloads/sprites/effects/extra_move$ convert out.png -transparent cyan extra_move.png
