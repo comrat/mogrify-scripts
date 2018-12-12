1c2b05fbe7e1153355eb530ca1124c89b458063b
Alexey Pobery <poberyaa@gmail.com>
2018-12-13 02:15:00 +0300

    Use optiona arguments as input

--- change_background.sh
+++ change_background.sh
@@ -1,13 +1,42 @@
 #! /bin/bash
 
-if [[ $# < 1 ]]; then
-	echo "Provide at least one parameter: output file"
-	exit 2
-fi
+INPUT_FILE="input.png"
+INPUT_COLOR="transparent"
+OUTPUT_FILE="output.png"
+OUTPUT_COLOR="transparent"
 
-INPUT_FILE=$1
-OUTPUT_FILE=$2
-INPUT_COLOR=$3
-OUTPUT_COLOR=$4
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--from-color)
+    INPUT_COLOR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--to-color)
+    OUTPUT_COLOR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 convert $INPUT_FILE -$OUTPUT_COLOR $INPUT_COLOR $OUTPUT_FILE
