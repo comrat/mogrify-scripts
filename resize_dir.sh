e050080c1078db196b8dc46349dddcc8f8f91a5d
Alexey Pobery <poberyaa@gmail.com>
2019-11-12 23:58:27 +0300

    Add resize dir script

---
+++ resize_dir.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+WIDTH=720
+HEIGHT=480
+OUTPUT_DIR="output"
+
+case $key in
+    -i|--input)
+    INPUT_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -w|--width)
+    WIDTH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--height)
+    HEIGHT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+for i in $INPUT_DIR/*;
+    do name=`echo $i | cut -d'.' -f1`;
+        FILENAME=$(basename "$i")
+        convert $FILENAME -resize "720x480" "$OUTPUT_DIR/$FILENAME"
+    done
