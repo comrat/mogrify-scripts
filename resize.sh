e9af262eb2c8a92edaa8fd416ee262b08e19cba4
Alexey Pobery <poberyaa@gmail.com>
2019-11-13 22:29:29 +0300

    Add resize script

---
+++ resize.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+WIDTH=100
+HEIGHT=100
+OUTPUT_FILE="output.png"
+
+case $key in
+    -i|--input)
+    INPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -o|--output)
+    OUTPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -w|--width)
+    WIDTH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--height)
+    HEIGHT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+CROP_ARG=$WIDTH"x"$HEIGHT"+0+0"
+echo $CROP_ARG
+convert $INPUT_FILE -resize "$WIDTHx$HEIGHT^" -gravity center -crop $CROP_ARG +repage $OUTPUT_FILE
