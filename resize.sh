03e950916d064558edb4e639dd0595f47fd7a22c
Alexey Pobery <poberyaa@gmail.com>
2019-11-21 00:46:22 +0300

    Fix resize script

--- resize.sh
+++ resize.sh
@@ -1,40 +1,39 @@
 #! /bin/bash
 
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
 WIDTH=100
 HEIGHT=100
-OUTPUT_FILE="output.png"
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    OUTPUT_FILE="output.png"
 
-case $key in
-    -i|--input)
-    INPUT_FILE="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -o|--output)
-    OUTPUT_FILE="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -w|--width)
-    WIDTH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -h|--height)
-    HEIGHT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
+    case $key in
+        -i|--input)
+        INPUT_FILE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -o|--output)
+        OUTPUT_FILE="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -w|--width)
+        WIDTH="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -h|--height)
+        HEIGHT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
