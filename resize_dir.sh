0f63ed4c9ea61da2f4ee53664800965c17fe223c
Alexey Pobery <poberyaa@gmail.com>
2019-11-23 23:35:03 +0300

    Implement size setting in resize_dir script

--- resize_dir.sh
+++ resize_dir.sh
@@ -1,45 +1,44 @@
 #! /bin/bash
 
 POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
 WIDTH=720
 HEIGHT=480
-OUTPUT_DIR="output"
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    OUTPUT_DIR="output"
 
-case $key in
-    -i|--input)
-    INPUT_DIR="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -o|--output)
-    OUTPUT_DIR="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -w|--width)
-    WIDTH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -h|--height)
-    HEIGHT="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
+    case $key in
+        -i|--input)
+        INPUT_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -o|--output)
+        OUTPUT_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -w|--width)
+        WIDTH="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -h|--height)
+        HEIGHT="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 for i in $INPUT_DIR/*;
     do name=`echo $i | cut -d'.' -f1`;
         FILENAME=$(basename "$i")
-        convert $FILENAME -resize "720x480" "$OUTPUT_DIR/$FILENAME"
+        convert $FILENAME -resize "$WIDTHxHEIGHT" "$OUTPUT_DIR/$FILENAME"
     done
